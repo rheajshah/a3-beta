5f61fdb5a0ffabcfea590fad85c290fc8c348ce6
eshub1 <eshitha.bangray@gmail.com>
2025-04-02 16:52:31 -0500

    reset password VC Swift file made

---
+++ A3_IOS/ResetPasswordViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ResetPasswordViewController.swift
+//  A3_IOS
+//
+//  Created by Eshitha B on 4/2/25.
+//
+
+import UIKit
+
+class ResetPasswordViewController: UIViewController {
+
+    @IBOutlet weak var newPassTextField: UITextField!
+    
+    @IBOutlet weak var resetPassTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func savePassPressed(_ sender: Any) {
+    }
+    
+}
