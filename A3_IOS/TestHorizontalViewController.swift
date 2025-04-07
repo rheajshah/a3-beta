ca85a862b32166f3749be1ef3b60fdadd3f3e540
eshub1 <eshitha.bangray@gmail.com>
2025-04-07 13:21:01 -0500

    horizontal view donee!! for comps tab

---
+++ A3_IOS/TestHorizontalViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TestHorizontalViewController.swift
+//  A3_IOS
+//
+//  Created by Eshitha B on 4/7/25.
+//
+
+import UIKit
+
+class TestHorizontalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBOutlet weak var myView: UIView!
+    
+    
+}
