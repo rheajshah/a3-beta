61f6333724238ed21100fa8f9c4526e04a38502d
eshub1 <eshitha.bangray@gmail.com>
2025-04-07 13:23:59 -0500

    test push for horizontal view

--- A3_IOS/CompetitionsViewController.swift
+++ A3_IOS/CompetitionsViewController.swift
@@ -7,15 +7,54 @@
 
 import UIKit
 
-class CompetitionsViewController: UIViewController {
+class CompetitionsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
     @IBOutlet var previousCompTableView: UITableView!
     @IBOutlet weak var profileButton: UIBarButtonItem!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let list: [Comp] = [
+            Comp(title: "Box 1", imageName: "box1"),
+            Comp(title: "Box 2", imageName: "box2"),
+            Comp(title: "Box 3", imageName: "box3"),
+            Comp(title: "Box 4", imageName: "box4")
+        ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return list.count
+    }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BoxCell", for: indexPath) as! BoxCell
+        let box = list[indexPath.item]
+        cell.compName.text = box.title
+       // cell.compImage.image = UIImage(named: box.imageName)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                            sizeForItemAt indexPath: IndexPath) -> CGSize {
+            return CGSize(width: 120, height: 160)
+        }
+}
+
+class BoxCell: UICollectionViewCell {
+    
+    @IBOutlet weak var compImage: UIImageView!
+    
+    @IBOutlet weak var compName: UILabel!
+    
+    
+}
+
+struct Comp {
+    let title: String
+    let imageName: String
 }
