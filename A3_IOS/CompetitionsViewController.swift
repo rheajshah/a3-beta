609c274d14ca857e1ea18b0f3bf443724a38388b
Aryan Samal <aryan.samal@utexas.edu>
2025-04-08 20:32:33 -0500

    Added upcoming comps firebase support + segue to comp desc

--- A3_IOS/CompetitionsViewController.swift
+++ A3_IOS/CompetitionsViewController.swift
@@ -16,7 +16,97 @@ struct PreviousComp {
     let logoRef: String
 }
 
+struct UpcomingComp {
+    let id: String
+    let name: String
+    let date: Date
+    let imageURL: String
+}
+
 class CompetitionsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UITableViewDelegate, UITableViewDataSource{
+    
+    @IBOutlet var previousCompTableView: UITableView!
+    @IBOutlet weak var profileButton: UIBarButtonItem!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var upcomingComps: [UpcomingComp] = []
+    var previousComps: [PreviousComp] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        previousCompTableView.dataSource = self
+        previousCompTableView.delegate = self
+        
+        populateComps()
+        previousCompTableView.reloadData()
+        collectionView.reloadData()
+    }
+    
+    func populateComps() {
+        let db = Firestore.firestore()
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateFormat = "MMMM dd yyyy"
+        let todayDate = Date()
+        
+        db.collection("comps").getDocuments() {
+            snapshot, error in
+            if let error = error {
+                print("Error while fetching comps: \(error)")
+            }
+            
+            
+            // Return if error while retrieving documents
+            guard let documents = snapshot?.documents else { return }
+            
+            var tempPrevComps: [PreviousComp] = []
+            var tempUpcomingComps: [UpcomingComp] = []
+            for document in documents {
+                let data = document.data()
+                guard
+                    let name = data["name"] as? String,
+                    let logoRef = data["logoURL"] as? String,
+                    let dateString = data["date"] as? String,
+                    let bannerRef = data["bannerURL"] as? String,
+                    let date = dateFormatter.date(from: dateString)
+                else {
+                    continue
+                }
+                
+                print("Date String: \(dateString)")
+                print("Date: \(date)")
+                
+                if date < todayDate {
+                    let comp = PreviousComp (
+                        id: document.documentID,
+                        name: name,
+                        date: date,
+                        logoRef: logoRef
+                    )
+                    tempPrevComps.append(comp)
+                } else {
+                    let comp = UpcomingComp (
+                        id: document.documentID,
+                        name: name,
+                        date: date,
+                        imageURL: bannerRef
+                    )
+                    tempUpcomingComps.append(comp)
+                }
+            }
+            self.previousComps = tempPrevComps.sorted(by: { $0.date > $1.date})
+            self.upcomingComps = tempUpcomingComps.sorted(by: { $0.date < $1.date})
+            print(self.upcomingComps)
+            DispatchQueue.main.async {
+                self.previousCompTableView.reloadData()
+                self.collectionView.reloadData()
+            }
+        }
+    }
+
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return previousComps.count
@@ -67,90 +157,22 @@ class CompetitionsViewController: UIViewController, UICollectionViewDataSource, 
         return 80
     }
     
-
-    @IBOutlet var previousCompTableView: UITableView!
-    @IBOutlet weak var profileButton: UIBarButtonItem!
-    @IBOutlet weak var collectionView: UICollectionView!
-    
-    let list: [Comp] = [
-            Comp(title: "Box 1", imageName: "box1"),
-            Comp(title: "Box 2", imageName: "box2"),
-            Comp(title: "Box 3", imageName: "box3"),
-            Comp(title: "Box 4", imageName: "box4")
-        ]
-    
-    var previousComps: [PreviousComp] = []
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        collectionView.dataSource = self
-        collectionView.delegate = self
-        
-        previousCompTableView.dataSource = self
-        previousCompTableView.delegate = self
-        
-        populatePreviousComps()
-    }
-    
-    func populatePreviousComps() {
-        let db = Firestore.firestore()
-        let dateFormatter = DateFormatter()
-        
-        dateFormatter.dateFormat = "MMMM dd yyyy"
-        let todayDate = Date()
-        
-        db.collection("comps").getDocuments() {
-            snapshot, error in
-            if let error = error {
-                print("Error while fetching comps: \(error)")
-            }
-            
-            
-            // Return if error while retrieving documents
-            guard let documents = snapshot?.documents else { return }
-            
-            var tempComps: [PreviousComp] = []
-            for document in documents {
-                let data = document.data()
-                guard
-                    let name = data["name"] as? String,
-                    let logoRef = data["logoURL"] as? String,
-                    let dateString = data["date"] as? String,
-                    let date = dateFormatter.date(from: dateString)
-                else {
-                    continue
-                }
-                
-                print("Date String: \(dateString)")
-                print("Date: \(date)")
-                
-                if date < todayDate {
-                    let comp = PreviousComp (
-                        id: document.documentID,
-                        name: name,
-                        date: date,
-                        logoRef: logoRef
-                    )
-                    tempComps.append(comp)
-                }
-            }
-            self.previousComps = tempComps.sorted(by: { $0.date > $1.date})
-            print(tempComps)
-            DispatchQueue.main.async {
-                self.previousCompTableView.reloadData()
-            }
-        }
-    }
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return list.count
+        return upcomingComps.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BoxCell", for: indexPath) as! BoxCell
-        let box = list[indexPath.item]
-        cell.compName.text = box.title
-       // cell.compImage.image = UIImage(named: box.imageName)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "UpcomingCompCell", for: indexPath) as! UpcomingCompCell
+        let comp = upcomingComps[indexPath.item]
+        cell.compName.text = comp.name
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        cell.date.text = dateFormatter.string(from: comp.date)
+        cell.imageView.image = nil
+        
+        if let logoURL = URL(string: comp.imageURL) {
+            loadImage(from: logoURL, into: cell.imageView)
+        }
         return cell
     }
     
@@ -158,18 +180,29 @@ class CompetitionsViewController: UIViewController, UICollectionViewDataSource, 
                             sizeForItemAt indexPath: IndexPath) -> CGSize {
             return CGSize(width: 120, height: 160)
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let selectedComp = upcomingComps[indexPath.row]
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let compInfoVC = storyboard.instantiateViewController(withIdentifier: "CompDescriptionViewController") as? CompDescriptionViewController {
+            compInfoVC.competitionID = selectedComp.id  //pass the comp ID to CompDescriptionViewController
+            self.navigationController?.pushViewController(compInfoVC, animated: true)
+        }
+        
+    }
 }
 
-class BoxCell: UICollectionViewCell {
-    
-    @IBOutlet weak var compImage: UIImageView!
-    
-    @IBOutlet weak var compName: UILabel!
-    
-    
-}
-
-struct Comp {
-    let title: String
-    let imageName: String
-}
+//class BoxCell: UICollectionViewCell {
+//    
+//    @IBOutlet weak var compImage: UIImageView!
+//    
+//    @IBOutlet weak var compName: UILabel!
+//    
+//    
+//}
+//
+//struct Comp {
+//    let title: String
+//    let imageName: String
+//}
---
+++ A3_IOS/UpcomingCompCell.swift
@@ -0,0 +1,25 @@
+//
+//  UpcomingCompCell.swift
+//  A3_IOS
+//
+//  Created by Aryan Samal on 4/8/25.
+//
+
+import Foundation
+import UIKit
+
+class UpcomingCompCell: UICollectionViewCell {
+        
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var compName: UILabel!
+    @IBOutlet weak var date: UILabel!
+    
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+    }
+    
+}
